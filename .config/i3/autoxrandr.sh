31137aa78ee86a6ffdf5259fe06ec7c711297110
Ian Seddon <ianseddon55@gmail.com>
2021-02-19 05:22:18 -0500

    Initial commit.

---
+++ .config/i3/autoxrandr.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Script to select the monitor automatically.
+# It works through i3wm and xrandr.
+#
+# Explanation:
+# - If external monitor is plugged on, it will turn the LCD screen off.
+# - To change the settings during an Xsession I use the tool lxrandr.
+
+for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
+  if [[ $output =~ ^eDP.*$ ]]; then
+    lvds=$output
+  fi
+donr
+
+for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
+  if [[ ! $output =~ ^eDP.*$ ]]; then
+    xrandr --output $lvds --off --output $output --primary --auto
+    feh --bg-fill ~/.wallpaper/firewatch-blue.png
+  else
+    xrandr --output $lvds --auto
+    feh --bg-fill ~/.wallpaper/firewatch-blue.png
+  fi
+done

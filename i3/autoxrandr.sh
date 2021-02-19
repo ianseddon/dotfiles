2632bebb67e21fae76b66bb108ef06516c52aebf
Ian Seddon <ianseddon55@gmail.com>
2021-02-19 14:27:10 -0500

    Use autorandr for display configuration.

--- i3/autoxrandr.sh
+++ i3/autoxrandr.sh
@@ -7,18 +7,20 @@
 # - If external monitor is plugged on, it will turn the LCD screen off.
 # - To change the settings during an Xsession I use the tool lxrandr.
 
-for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
-  if [[ $output =~ ^eDP.*$ ]]; then
-    lvds=$output
-  fi
-donr
+#for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
+#  if [[ $output =~ ^eDP.*$ ]]; then
+#    lvds=$output
+#  fi
+#donr
 
-for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
-  if [[ ! $output =~ ^eDP.*$ ]]; then
-    xrandr --output $lvds --off --output $output --primary --auto
-    feh --bg-fill ~/.wallpaper/firewatch-blue.png
-  else
-    xrandr --output $lvds --auto
-    feh --bg-fill ~/.wallpaper/firewatch-blue.png
-  fi
-done
+#for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
+#  if [[ ! $output =~ ^eDP.*$ ]]; then
+#    xrandr --output $lvds --off --output $output --primary --auto
+#    feh --bg-fill ~/.wallpaper/firewatch-blue.png
+#  else
+#    xrandr --output $lvds --auto
+#    feh --bg-fill ~/.wallpaper/firewatch-blue.png
+#  fi
+#done
+
+autorandr --change

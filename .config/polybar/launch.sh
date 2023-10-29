46cd5759a701c9f4e163c6b527371928f14f6223
Ian Seddon <ianseddon55@gmail.com>
2023-10-29 10:38:44 -0600

    Configure polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+if [ "$1" == "light" ]
+then
+	polybar -c $HOME/.config/polybar/bars/light-config nord-top &
+	polybar -c $HOME/.config/polybar/bars/light-config nord-down &
+else
+	polybar -c $HOME/.config/polybar/bars/dark-config nord-top &
+	polybar -c $HOME/.config/polybar/bars/dark-config nord-down &
+fi
 
 echo "Bars launched..."

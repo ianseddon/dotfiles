945ebb6e3153922829899580c0798601d00ef2e0
Ian Seddon <iseddon@ebsco.com>
2025-09-30 16:10:33 -0400

    nvim config updates

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- required by magma-nvim
--- vim.g.python3_host_prog = vim.fn.exepath("python3")
+-- Don't format with prettier unless a config is present.
+vim.g.lazyvim_prettier_needs_config = true
